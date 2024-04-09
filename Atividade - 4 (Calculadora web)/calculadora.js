101e98b9798477c654f12ab214d7c810946e283c
Thiago-engenheiro <thiago.principal11@outlook.com.br>
2024-04-09 12:25:12 -0300

    update

---
+++ Atividade - 4 (Calculadora web)/calculadora.js
@@ -0,0 +1,38 @@
+/* 
+ 
+    Criador: Thiago Abraao
+    Data inicial do inicio da atividade: 09/04/2024
+    Data final do fim da atividade:  vazio/vazio/2024
+    Objetivo: Aprendizado de JavaScript
+    O que progama faz: calculadora web
+
+*/
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Instruções da atividade:
+
+  1 - implemente uma aplicação web com as rotas:
+
+    /somar/:a/:b
+    /subtrair/:a/:b
+    /multiplicar/:a/:b
+    /dividir/:a/:b
+
+    Que devem retornar o valor equivalente da operação considerando os valores a e b de entrada pela URL.
+
+  2 - A aplicação deve conter um arquivo index.js na raiz e um módulo calculadora.js na pasta util.
+  
+*/
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Declaração de Variaveis  */
+
+
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Codigo */
+
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Fim  */
+
