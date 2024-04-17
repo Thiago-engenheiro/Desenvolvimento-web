1d7cf91f153130d68f5a6702b2b5cf49c0b8ee14
Thiago-engenheiro <thiago.principal11@outlook.com.br>
2024-04-17 15:06:02 -0300

    Nova atividade

---
+++ Atividade - 5 ()/Lojinha.js
@@ -0,0 +1,35 @@
+/* 
+ 
+    Criador: Thiago Abraao
+    Data inicial do inicio da atividade: 17/04/2024
+    Data final do fim da atividade:  vazio/vazio/2024
+    Objetivo: Aprendizado de JavaScript
+    O que progama faz: Simula um estoque de uma loja 
+
+*/
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Instruções da atividade:
+
+  1 Implemente um aplicativo web com as seguintes rotas:
+
+  '/adicionar/:id/:nome/:qtd' - Adiciona um novo produto ao estoque
+  '/listar' - Lista todos os produtos do estoque
+  '/remover/:id' - Remove um produto do estoque
+  '/editar/:id/:qtd' - Altera a quantidade de um produto do estoque
+  Como solução, envie o link da pasta atividade5.
+
+*/
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Declaração de Variaveis  */
+
+
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Codigo */
+
+
+/*  __________________________________________________________________________________________________________________ */
+/*  Fim  */
+
